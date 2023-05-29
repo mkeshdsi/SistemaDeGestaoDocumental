ad1edb3c4498aa76d4ddd90b8140f6d29a0bd141
Vanio Anibal Macamo <macamovanioanibal@gmail.com>
2023-05-29 16:53:57 +0200

    Dashboard

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\User;
 
 use App\Models\Documento;
 
@@ -19,7 +20,36 @@ class Controller extends BaseController
 
     public function index_documentos(){
         $documentos = Documento::all();
-    
         return view('documentos.index', compact('documentos'));
+    }
+
+    public function dashboard(){
+            $totalUsers = 0;
+            $totalUserAdmin = 0;
+            $totalUserCorporativos = 0;
+            $totalUserIndividual = 0;
+           
+            return view ('dashboard') 
+                ->with('totalUsers',$totalUsers)
+                ->with('totalUserAdmin',$totalUserAdmin)
+                ->with('totalUserCorporativos',$totalUserCorporativos)
+                ->with('totalUserIndividual',$totalUserIndividual);
+
+        return view('dashboard');
+
+      /* if (auth()->check() && (auth()->user()->can('is_admin') || auth()->user()->can('is_corporate'))){
+            $totalUsers = User::count();
+            $totalUserAdmin = User::where('profile',"Admnistrador")->count();
+            $totalUserCorporativos = User::where('profile','Corporativo')->count();
+            $totalUserIndividual = User::where('profile','Individual')->count();
+           
+            return view ('dashboard') 
+                ->with('totalUsers',$totalUsers)
+                ->with('totalUserAdmin',$totalUserAdmin)
+                ->with('totalUserCorporativos',$totalUserCorporativos)
+                ->with('totalUserIndividual',$totalUserIndividual);
+        }else{
+            return redirect()->route('home',['users' => $users]);
+        } */
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,12 @@ Route::get('/', function () {
 Route::get('/entradaDocumentos',[App\Http\Controllers\Controller::class,'entradaDocumentos']);
 Route::get('/index_documentos',[App\Http\Controllers\Controller::class,'index_documentos']);
 
+Route::get('dashboard', [App\Http\Controllers\Controller::class, 'dashboard'])->name('dashboard');
+
 Route::post('/upload',[App\Http\Controllers\UploadController::class,'upload'])->name('upload');
 Route::post('/store',[App\Http\Controllers\UploadController::class,'store'])->name('store');
-//Route::post('/download-arquivos/{id}', 'UploadController@downloadArquivos')->name('download.arquivos');
-//Route::post('/download-arquivos/{id}',[App\Http\Controllers\UploadController::class,'downloadArquivos'])->name('download.arquivos');
-
 Route::get('/download-documento/{id}', [App\Http\Controllers\UploadController::class,'downloadDocumento'])->name('documento.download');
 Route::get('/download-formulario/{id}', [App\Http\Controllers\UploadController::class,'downloadFormulario'])->name('formulario.download');
-
-Route::get('/teste', function(){
-    $filePath = 'C:\Documentos_Sistema_Gestao_Documental\Formulario\teste.txt';
-    $fileName = 'teste.txt';
-
-    return Response::download($filePath, $fileName);
-});
 
 
 
